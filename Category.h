0883b7ed7319f0b40383e5ff3e4dc8601319ac4b
Paradoxon <two4god@gmail.com>
2015-08-15 23:38:46 +0200

    preparation to implement a Categorie Class

--- Category.h
+++ Category.h
@@ -1,11 +1,44 @@
+#ifndef CATEGORY_H
+#define CATEGORY_H
+
+
 #include <Archivable.h>
 #include <String.h>
 
 
 class Category {
-	public:
-		Category(char *category);
+
+public:
+							Category() {};
+							Category(const char* newName) {name=newName;};
+							~Category() {};
 	
-	private:
-		BString *category;
-}
+	const	char*			Name(void) {return name.String();};
+			void			SetName(BString newName){name.SetTo(newName);};
+	
+			time_t			LastUpdate(void){return updated;};
+			void			SetLastUpdate(bigtime_t newTime){ updated=newTime; };
+
+			int32			ID(void){return id;};
+			void			SetID(int32 newID){id=newID;};
+
+			BString			URL(void){return url.String();};
+			void			SetURL(BString newURL){url=newURL;};
+
+			
+			bool			operator==(const Category& task) const {};
+
+protected:
+			void			Init(void){}
+
+		
+private:
+			BString			name;
+			time_t			updated; //this should be hidden at least in the tracker
+			
+			// we should implement the id, etag, selfLink
+			int32			id;
+			BString			url;
+};
+
+#endif
