1bdacc226fc81abfed8eeb80e79abc2a59d9f83a
Paradoxon <two4god@gmail.com>
2015-12-01 21:58:58 +0100

    reworked TaskSync Class a little bit

--- src/TaskSync.h
+++ src/TaskSync.h
@@ -19,22 +19,38 @@ class TaskSync : public BHandler {
 	 *here you should do things like prepare everything login and so on
 	 *returns the status of the how far the Init came.
 	 */
-	virtual	status_t				Init(void)		= 0;
+	virtual	status_t				Init(void)					= 0;
 	
-	/**
-	 * returns a List of BString of all Categories
-	 */
-	virtual	BObjectList<Category>*	GetCategories()				= 0;
 	
 	/**
 	 * returns a List of Task Objects 
 	 */
 	virtual	BObjectList<Task>*		GetTasks() 					= 0;
+	virtual	BObjectList<Task>*		GetTasks(Category ctgr)		= 0;
 	
-	virtual status_t				UpdateTasks(BObjectList<Task>*) = 0;
+	virtual	Task*					GetTask(BString id) 		= 0;
 	
-	virtual status_t				UpdateCategories(BObjectList<Category>*) = 0;
 	
+	virtual status_t				AddTask(Task *tsk)			= 0;
+	
+	virtual status_t				UpdateTask(BString id,Task *tsk) = 0;
+	
+	virtual status_t				RemoveTask(BString id)		= 0;
+	
+	
+	/**
+	 * returns a List of BString of all Categories
+	 */
+	virtual	BObjectList<Category>*	GetCategories()					= 0;
+
+	virtual	Category*				GetCategorie(BString id)		= 0;
+
+	virtual status_t				AddCategorie(Category *ctgr)	= 0;	
+	
+	virtual status_t				UpdateCategorie(BString id,Category *ctgr)	= 0;
+
+	//what should we do? should we delete all task wich belong to this cateorie?
+	virtual status_t				RemoveCategorie(BString id)		= 0;
 	
 };
 
